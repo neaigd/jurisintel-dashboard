b9d4e44e7dbfe1f972ccdca0f8d7e49bcc520f28
Luiz Peixoto <peixoto@riseup.net>
2025-06-24 15:20:04 -0300

    Update files: modifica o docker-compose atualiza o docker-setup.sh tenta mofificações nos prompts

--- docker-setup.sh
+++ docker-setup.sh
@@ -15,17 +15,22 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Run the Docker container
+# Run the Docker container in the foreground
 echo "Starting Docker container on port ${APP_PORT}..."
-docker-compose up -d
+
+echo "JurisIntel Dashboard is starting up in a Docker container."
+echo "It should be available at http://localhost:${APP_PORT} shortly."
+# Wait a bit for services to come up before opening the browser
+sleep 5
+# Open the application URL in the default browser
+xdg-open http://localhost:${APP_PORT}
+
+docker-compose up
 
 if [ $? -ne 0 ]; then
     echo "Docker compose up failed!"
     exit 1
 fi
-
-echo "JurisIntel Dashboard is starting up in a Docker container."
-echo "It should be available at http://localhost:${APP_PORT} shortly."
 
 # Add a cleanup function
 cleanup() {
@@ -37,10 +42,6 @@ cleanup() {
 # Set trap to call cleanup function on script exit or interruption
 trap cleanup EXIT INT TERM
 
-# Keep the script running in the foreground to allow cleanup on Ctrl+C
 echo "Press Ctrl+C to stop the application and clean up resources."
-
-# Wait indefinitely for Ctrl+C
-while true; do
-    sleep 1
-done
+# docker-compose up runs in the foreground, so no need for an infinite loop here.
+# The trap will handle cleanup if the script is interrupted.
